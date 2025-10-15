d8236c4e3bb13629bc05a547bbfb1a1e44e257c6
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-10-15 17:30:05 +0300

    Use an iterator to build `Config`

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use std::process;
 use minigrep::{search, search_case_insensitive};
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // `env::args()` returns an iterator, pass the ownership to `Config::build`
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -41,12 +41,19 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // Ignore the program name which is the first value in args
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         //  We don’t care about the value of the environment variable,
         // just whether it’s set or unset.
