358b27e86e5efcb425b857d300eeb29922cfc5ec
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-10-15 15:14:17 +0300

    Implement `search` and `search_case_insensitive`

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use std::fs;
 use std::process;
 
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -11,9 +11,6 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
         println!("Application error: {e}");
@@ -24,7 +21,13 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else  {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -33,7 +36,8 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 struct Config {
     query: String,
-    file_path: String
+    file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -44,6 +48,10 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        //  We don’t care about the value of the environment variable,
+        // just whether it’s set or unset.
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
